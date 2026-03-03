c27506fb9b8f25c8ef94a61896c1c8ee5ecf0762
vikas jagtap <vikasjagtap.9996@gmail.com>
2026-03-03 22:27:06 +0530

    db madi chnages

--- server/db.js
+++ server/db.js
@@ -12,7 +12,7 @@ const db = mysql.createPool({
 
 db.getConnection((err, connection) => {
     if (err) {
-        console.error('Database connection failed:', err.message);
+        console.error('Database connection failed:', err);
     } else {
         console.log('Connected to MySQL database');
         connection.release();
