b85d4e377629427e2d560558bdf7d1b8c7400500
vikas jagtap <vikasjagtap.9996@gmail.com>
2026-03-03 22:46:53 +0530

    feat: initialize MySQL database connection pool for MySQL2.

--- server/db.js
+++ server/db.js
@@ -1,6 +1,13 @@
 const mysql = require('mysql2');
 
-const db = mysql.createPool(process.env.MYSQL_PUBLIC_URL);
+const db = mysql.createPool({
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT,
+    connectionLimit: 10
+});
 
 db.getConnection((err, connection) => {
     if (err) {
